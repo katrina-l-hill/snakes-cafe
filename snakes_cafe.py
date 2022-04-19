d597a17d88334cb2783a24b1d21ccc3399a9fa5f
Katrina Hill <khill365@gmail.com>
2022-04-18 18:18:45 -0700

    added text for menu

---
+++ snakes_cafe.py
@@ -0,0 +1,50 @@
+intro = """
+**************************************
+** Welcome to the Snakes Cafe! **
+** Please see our menu below. **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+"""
+response = input('> ')
+print(response)
+
+appetizers = ('Wings', 'Cookies', 'Spring Rolls')
+
+entrees = ('Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden')
+
+desserts = ('Ice Cream', 'Cake', 'Pie')
+
+drinks = ('Coffee', 'Tea', 'Unicorn Tears')
+
+# start program
+print(intro)
